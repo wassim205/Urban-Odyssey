0f1009fe7e574c501d7ebaf8b5ac37fe15eac962
wassim205 <wassimelmorabit7@gmail.com>
2025-03-25 11:17:49 +0000

    UD-30 fixing auth function a simple register page for testing

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,20 +1,16 @@
-import './bootstrap';
-import './components/Home';
+import "./bootstrap";
+import "./components/Home";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./components/Home";
+import RegisterForm from "./components/RegisterForm";
 
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './components/Home';
-import Test from './components/TEST';
 
-const container = document.getElementById('root');
+// for register page
+const container = document.getElementById("register");
 if (container) {
     const root = ReactDOM.createRoot(container);
-    root.render(<Home />);
-}
-
-const container2 = document.getElementById('root2');
-if (container2) {
-    const root2 = ReactDOM.createRoot(container2);
-    root2.render(<Test />);
+    root.render(<RegisterForm />);
 }
