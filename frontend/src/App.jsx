cb7c95f5ddc99cca35053441cd09a3e31d5d3a3d
wassim205 <wassimelmorabit7@gmail.com>
2025-04-14 22:36:49 +0100

    modifications

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css'
+import Home from './components/Home';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+export default App
