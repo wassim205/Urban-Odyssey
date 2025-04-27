8f1169c397207e87230b5028ec95b89da53e655d
wassim205 <wassimelmorabit7@gmail.com>
2025-04-27 03:45:11 +0100

    implementaion de AOS pour avoir une bonne UX dans About page

--- frontend/src/components/About/MainContent.jsx
+++ frontend/src/components/About/MainContent.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import city from "./../../images/City.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function MainContent() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: false,
+      mirror: true,
+      offset: 120,
+    });
+  }, []);
+
   return (
     <div className="">
-      <h1 className="font-bebas text-[#313131] text-center mt-6 md:mt-12 text-4xl md:text-5xl lg:text-6xl px-4">
+      <h1
+        data-aos="fade-up"
+        data-aos-delay="100"
+        className="font-bebas text-[#313131] text-center mt-6 md:mt-20 text-4xl md:text-5xl lg:text-6xl px-4"
+      >
         À Propos de Urban Odyssey
       </h1>
 
       <div className="grid gap-10 md:gap-20">
         {/* First section */}
-        <div className="px-6 md:ml-14 mt-6 md:mt-12 space-y-4 md:space-y-6">
-          <h2 className="text-[#A45A3D] font-bebas text-3xl md:text-4xl lg:text-5xl">
+        <div className="px-6 md:ml-14 mt-6 md:mt-8 space-y-4 md:space-y-6">
+          <h2
+            data-aos="fade-right"
+            data-aos-delay="200"
+            className="text-[#A45A3D] font-bebas text-3xl md:text-4xl lg:text-5xl"
+          >
             Qui suis-je ?
           </h2>
-          <p className="text-[#313131] font-gentiumBook text-lg md:text-2xl lg:text-4xl md:mr-16">
+          <p
+            data-aos="fade-up"
+            data-aos-delay="300"
+            className="text-[#313131] font-gentiumBook text-lg md:text-2xl lg:text-4xl md:mr-16"
+          >
             Je m'appelle Wassim El Mourabit, un passionné de technologie et de
             développement web. Ayant toujours été intéressé par l'exploration et
             la découverte, j'ai conçu Urban Odyssey pour offrir aux voyageurs et
@@ -27,6 +50,7 @@ function MainContent() {
 
         {/* City background section */}
         <div
+          data-aos="zoom-in-up"
           className="relative bg-cover bg-center overflow-hidden w-full"
           style={{ backgroundImage: `url(${city})` }}
         >
@@ -36,10 +60,17 @@ function MainContent() {
           <div className="relative z-10">
             {/* Pourquoi Urban Odyssey */}
             <div className="p-6 md:p-14 space-y-4 md:space-y-8">
-              <h2 className="font-bebas text-3xl md:text-4xl lg:text-5xl text-[#D8C292]">
+              <h2
+                data-aos="fade-right"
+                className="font-bebas text-3xl md:text-4xl lg:text-5xl text-[#D8C292]"
+              >
                 Pourquoi Urban Odyssey ?
               </h2>
-              <p className="font-gentiumBook text-white text-lg md:text-2xl lg:text-4xl">
+              <p
+                data-aos="fade-up"
+                data-aos-delay="100"
+                className="font-gentiumBook text-white text-lg md:text-2xl lg:text-4xl"
+              >
                 De nombreuses personnes, qu'elles soient touristes ou habitants,
                 visitent souvent les mêmes attractions populaires sans explorer
                 toute la richesse qu'une ville a à offrir. Urban Odyssey est né
@@ -54,10 +85,17 @@ function MainContent() {
 
             {/* Notre Vision */}
             <div className="p-6 md:p-14 space-y-4 md:space-y-8">
-              <h2 className="font-bebas text-3xl md:text-4xl lg:text-5xl text-[#D8C292]">
+              <h2
+                data-aos="fade-right"
+                className="font-bebas text-3xl md:text-4xl lg:text-5xl text-[#D8C292]"
+              >
                 Notre Vision
               </h2>
-              <p className="font-gentiumBook text-white text-lg md:text-2xl lg:text-4xl">
+              <p
+                data-aos="fade-up"
+                data-aos-delay="100"
+                className="font-gentiumBook text-white text-lg md:text-2xl lg:text-4xl"
+              >
                 Urban Odyssey ne se contente pas de répertorier les lieux
                 emblématiques ; il propose une expérience immersive qui valorise
                 la culture locale et les trésors cachés. Nous souhaitons rendre
@@ -70,27 +108,34 @@ function MainContent() {
 
             {/* Nos Objectifs */}
             <div className="p-6 md:p-14 space-y-4 md:space-y-8">
-              <h2 className="font-bebas text-3xl md:text-4xl lg:text-5xl text-[#D8C292]">
+              <h2
+                data-aos="fade-right"
+                className="font-bebas text-3xl md:text-4xl lg:text-5xl text-[#D8C292]"
+              >
                 Nos Objectifs
               </h2>
-              <div className="font-gentiumBook text-white text-lg md:text-2xl lg:text-4xl">
+              <div
+                data-aos="fade-up"
+                data-aos-delay="100"
+                className="font-gentiumBook text-white text-lg md:text-2xl lg:text-4xl"
+              >
                 <ul className="list-disc pl-6 space-y-2 md:space-y-4">
-                  <li>
+                  <li data-aos="fade-up" data-aos-delay="150">
                     Offrir des recommandations pertinentes et personnalisées.
                   </li>
-                  <li>
+                  <li data-aos="fade-up" data-aos-delay="200">
                     Permettre aux utilisateurs de trouver des informations de
                     transport en temps réel.
                   </li>
-                  <li>
+                  <li data-aos="fade-up" data-aos-delay="250">
                     Intégrer des lieux culturels et religieux pour une
                     exploration enrichie.
                   </li>
-                  <li>
+                  <li data-aos="fade-up" data-aos-delay="300">
                     Proposer des avis et conseils d'autres utilisateurs pour une
                     expérience collaborative.
                   </li>
-                  <li>
+                  <li data-aos="fade-up" data-aos-delay="350">
                     Fournir une interface intuitive et rapide pour faciliter la
                     navigation.
                   </li>
@@ -102,22 +147,29 @@ function MainContent() {
 
         {/* Pour Qui section */}
         <div className="px-6 md:ml-14 space-y-4 md:space-y-6">
-          <h2 className="text-[#A45A3D] font-bebas text-3xl md:text-4xl lg:text-5xl">
+          <h2
+            data-aos="fade-right"
+            className="text-[#A45A3D] font-bebas text-3xl md:text-4xl lg:text-5xl"
+          >
             Pour Qui ?
           </h2>
           <div className="font-gentiumBook text-lg md:text-2xl lg:text-4xl text-[#313131]">
-            <p className="mb-2">
+            <p data-aos="fade-up" data-aos-delay="100" className="mb-2">
               Urban Odyssey s'adresse à tous ceux qui souhaitent explorer une
               ville autrement :
             </p>
             <ul className="list-disc pl-16 space-y-2 md:space-y-4">
-              <li>Touristes en quête de nouvelles découvertes.</li>
-              <li>Résidents locaux voulant redécouvrir leur propre ville.</li>
-              <li>
+              <li data-aos="fade-up" data-aos-delay="150">
+                Touristes en quête de nouvelles découvertes.
+              </li>
+              <li data-aos="fade-up" data-aos-delay="200">
+                Résidents locaux voulant redécouvrir leur propre ville.
+              </li>
+              <li data-aos="fade-up" data-aos-delay="250">
                 Voyageurs solo, en famille ou entre amis cherchant des
                 recommandations adaptées à leurs préférences.
               </li>
-              <li>
+              <li data-aos="fade-up" data-aos-delay="300">
                 Entreprises locales désireuses de se faire connaître et
                 d'attirer plus de visiteurs.
               </li>
@@ -127,36 +179,39 @@ function MainContent() {
 
         {/* Nos Fonctionnalités Clés section */}
         <div className="px-6 md:ml-14 space-y-4 md:space-y-6">
-          <h2 className="text-[#A45A3D] font-bebas text-3xl md:text-4xl lg:text-5xl">
+          <h2
+            data-aos="fade-right"
+            className="text-[#A45A3D] font-bebas text-3xl md:text-4xl lg:text-5xl"
+          >
             Nos Fonctionnalités Clés
           </h2>
           <div className="font-gentiumBook text-lg md:text-2xl lg:text-4xl text-[#313131]">
             <ul className="list-disc pl-8 space-y-2 md:space-y-4">
-              <li>
+              <li data-aos="fade-up" data-aos-delay="150">
                 <span className="font-semibold">
                   Recommandations Personnalisées :
                 </span>{" "}
                 Découvrez des lieux adaptés à vos goûts et centres d'intérêt.
               </li>
-              <li>
+              <li data-aos="fade-up" data-aos-delay="200">
                 <span className="font-semibold">
                   Informations sur les Transports :
                 </span>{" "}
                 Accédez à des itinéraires, tarifs et options de transport
                 pratiques.
               </li>
-              <li>
+              <li data-aos="fade-up" data-aos-delay="250">
                 <span className="font-semibold">
                   Avis et Notes des Utilisateurs :
                 </span>{" "}
                 Partagez vos expériences et consultez les retours d'autres
                 explorateurs.
               </li>
-              <li>
+              <li data-aos="fade-up" data-aos-delay="300">
                 <span className="font-semibold">Cartes Interactives :</span>{" "}
                 Naviguez facilement et planifiez vos trajets en un clic.
               </li>
-              <li>
+              <li data-aos="fade-up" data-aos-delay="350">
                 <span className="font-semibold">Contenu Collaboratif :</span>{" "}
                 Contribuez à enrichir la plateforme en ajoutant de nouveaux
                 lieux et avis.
@@ -168,7 +223,7 @@ function MainContent() {
         {/* Footer with conclusion text overlapping */}
         <div className="relative">
           {/* SVG Footer */}
-          <div className="w-full">
+          <div className="w-full" data-aos="fade-up">
             <img
               src="/src/assets/footer/footer.svg"
               alt="footer"
@@ -178,7 +233,10 @@ function MainContent() {
 
           {/* Overlapping conclusion text */}
           <div className="absolute top-2/4 left-0 right-0 z-10 px-6 md:px-14 text-center">
-            <p className="font-bebas text-[#D8C292] text-2xl md:text-3xl lg:text-4xl max-w-full mx-auto drop-shadow-2xl">
+            <p
+              data-aos="zoom-in"
+              className="font-bebas text-[#D8C292] text-2xl md:text-3xl lg:text-4xl max-w-full mx-auto drop-shadow-2xl"
+            >
               Urban Odyssey est plus qu'un simple guide touristique, c'est une
               invitation à l'aventure urbaine, à la découverte et à
               l'exploration personnalisée. Rejoignez-nous et transformez chaque
